ccb6ce64a1c2577d0ce768ff82d10eea105b8f94
bboy <9727215+bboy_9@user.noreply.gitee.com>
2026-06-02 12:56:48 +0800

    Remove project folders from remote repository (keep local copies)

--- vip_homework/smartbox/monitor/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Camera
-
-class CameraAdmin(admin.ModelAdmin):
-    list_display = ('name','rtsp','position','is_active','create_time')
-    list_per_page = 15
-
-admin.site.register(Camera,CameraAdmin)
